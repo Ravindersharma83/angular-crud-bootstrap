60bfbbef26eea0b24fcdd598eb00ab2355d1189e
ravindersharma83 <ravinder200198@gmail.com>
2023-06-06 17:35:15 +0530

    fetching records through api

--- src/app/Service/student.service.ts
+++ src/app/Service/student.service.ts
@@ -1,6 +1,14 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface StudentResponse {
+  "id": number,
+  "name": string,
+  "course": string,
+  "email": string,
+  "phone": number | string
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,5 +18,9 @@ export class StudentService {
 
   saveStudent(inputData : object){
     return this.httpClient.post(`http://localhost:3000/students`,inputData);
+  }
+
+  getStudents(){
+    return this.httpClient.get<StudentResponse[]>(`http://localhost:3000/students`);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentResponse, StudentService } from '../Service/student.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private studentService : StudentService){}
 
+  students !: StudentResponse[];
+  isLoading : boolean = false;
+
+  ngOnInit(){
+    this.getStudentLists();
+  }
+
+  getStudentLists(){
+    this.isLoading = true;
+    this.studentService.getStudents().subscribe({
+      next:(res) =>{
+        this.students = res;
+        this.isLoading = false;
+      },
+      error:(err:any)=>{
+        console.log(err);
+      }
+    });
+  }
 }
